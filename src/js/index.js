b59ce0e6d4f6969cb2dfabd1cb6961acad6e6099
Rex <464276167@qq.com>
2018-04-12 18:01:36 +0800

    完成首页main部分的商品轮播（有部分bug）

--- src/js/index.js
+++ src/js/index.js
@@ -9,23 +9,47 @@ require(['config'],function(){
                 url:'api/getdata.php',
                 success:function(data){
                     let res =data.slice(0, 20);
-                    console.log(res);
+                    // console.log(res);
                     var ul = document.createElement('ul');
+                    ul.classList.add('clearfix');
                     var main_focus = document.getElementById('main_focus')                  
                     ul.innerHTML = res.map(function(item){
                         item.url=item.url.replace('../','');
                         // console.log(item.url);
-                        return `<li>
+                        return `<a href="#" class="clearfix"><li>
                         <div class="photo"><img src="${item.url}"/></div>
                         <p class="descirbe">${item.descirbe}</p>
-                        <p class="detail"><span class="price">￥${item.price}</span><span class="sold">销量：${item.sold}</span></p>
-                        </li>`
+                        <p class="detail"><span class="price">￥${item.price}</span><span class="sold fr">销量：${item.sold}</span></p>
+                        </li></a>`
                     }).join('');
                     main_focus.appendChild(ul);
                 }
-            })
+        })
+        
+        // $main_focus.ajaxComplete(function(){ //待请求完成时 执行
+        // window.onload = function(){
+            let $main_focus = $('#main_focus');
+            console.log($main_focus)
+            let $turn = $('.turn');
+            let $left = $turn.children('.left');
+            let $right = $turn.children('.right');
+            let $ul = $main_focus.children('ul');
+            console.log($ul)
+            $main_focus.mouseover(()=>{
+                $turn.show(300).css({color:'#fc0'});
+            });
+            // $main_focus.mouseout(function(){
+            //     $turn.hide();
+            // })
+            $left.click(function(){
+
+            });
+        // }
+            
+        // });
         
         
+
     })();
     
 });
