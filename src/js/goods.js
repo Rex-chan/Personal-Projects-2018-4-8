82cf27efe347c67693db706b07efd93ebd67afe5
Rex <464276167@qq.com>
2018-04-13 21:22:03 +0800

    goods.html 读取数据前

--- src/js/goods.js
+++ src/js/goods.js
@@ -1,5 +1,5 @@
 require(['config'],function(){
-    require(['jquery','common','xcarousel'],function($){
+    require(['jquery','common'],function($){
         let $ul = $('#s_nav').find('ul');
         console.log($ul);
         $ul.on('mouseover','li',function(){
@@ -8,6 +8,20 @@ require(['config'],function(){
         $ul.on('mouseleave','li',function(){
             $(this).find('.show').slideToggle(200);
         });
+        ajax({
+            console.log(66)
+            url:'../api/getdata.php',
+            success:function(data){
+                    console.log(data);
+                    var ul = document.createElement('ul');
+                                      
+                    ul.innerHTML = res.map(function(item){
+                        
+                        
+                    }).join('');
+                    
+            }
+        })
         
         
         
