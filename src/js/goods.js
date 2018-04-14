f6a780b4c1f936121c53b707bbf1aabc5729025c
Rex <464276167@qq.com>
2018-04-14 12:10:03 +0800

    goods.html 排序和底部完成

--- src/js/goods.js
+++ src/js/goods.js
@@ -12,29 +12,52 @@ require(['config'],function(){
             url:'../api/getdata.php',
             success:function(data){
                     console.log(data);
-                    var main_product = document.getElementById('main_product');
-                    console.log(main_product); 
-                                    
-                    main_product.innerHTML = data.map(function(item){
-                        return `<div>
-                            <div class="photo">
-                                <img src="${item.url}"/>
-                            </div>
-                            <p class="descirbe">
-                                ${item.descirbe}
-                            </p>
-                            <p class="detail">
-                                <span class="price">￥${item.price}</span>
-                                <span class="sold fr">销量：${item.sold}</span>
-                            </p>
-                        </div>`                       
-                    }).join('');
+                    output();
+                    function output(){
+                        var main_product = document.getElementById('main_product');
+                        console.log(main_product); 
+                        main_product.innerHTML = data.map(function(item){
+                            return `<div class="product_item fl"><a href="#">
+                                <div class="photo">
+                                    <img src="${item.url}"/>
+                                </div>
+                                <p class="descirbe">
+                                    ${item.descirbe}
+                                </p>
+                                <p class="detail">
+                                    <span class="price">￥${item.price}</span>
+                                    <span class="sold fr">销量：${item.sold}</span>
+                                </p>
+                                </a></div>`                       
+                        }).join('');
+                    }
                     
+                    let $btn1 = $('button').eq(0)
+                    console.log($btn1);
+                    $btn1.click(function(){
+                        pJiang()
+                        output();
+                    })
+                    let $btn2 = $('button').eq(1)
                     
+                    $btn2.click(function(){
+                        sJiang()
+                        output();
+                    })
+                    function pJiang(){
+                        data.sort(function(a,b){
+                            return b.price - a.price;
+                        })
+                    }
+                    function sJiang(){
+                        data.sort(function(a,b){
+                            return b.sold - a.sold;
+                        })
+                    }
+                                
             }
         })
-        
-        
+
         
         
 
