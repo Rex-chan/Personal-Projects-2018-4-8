50dc181550b7c4008e1c2a8631afe63f2e1c178d
Rex <464276167@qq.com>
2018-04-15 17:55:33 +0800

    shoppingcar.css complete

--- src/js/details.js
+++ src/js/details.js
@@ -4,10 +4,17 @@ require(['config'],function(){
         let params = decodeURI(location.search).slice(1);
         
         let id = params.split('=')[1];
-        
+        let $ul = $('#s_nav').find('ul');
+        console.log($ul);
+        $ul.on('mouseover','li',function(){
+            $(this).find('.show').show();
+            
+        });
+        $ul.on('mouseleave','li',function(){
+            $(this).find('.show').slideToggle(200);
+        });
         $.ajax({
             url: '../api/getdata.php',
-
             success:function(data){                
                 let res = JSON.parse(data);
                 console.log(typeof(res));
@@ -24,12 +31,17 @@ require(['config'],function(){
                         <div class="price">
                         寺库价<span>￥${item.price}</span>
                         </div>
-                        <div>发货地<span>大陆有货预计5-10个工作日内送达</span></div>
-                        <div>温馨提示<span>本商品无质量问题不支持退换货 </span></div>
+                        <div class="text">发货地<span>大陆有货预计5-10个工作日内送达</span></div>
+                        <div class="text">温馨提示<span>本商品无质量问题不支持退换货 </span></div>
+                        <div class="buy">
+                            <button >立刻购买</button>
+                            <button ><i  class="icon-gouwuche iconfont"></i>加入购物车</button>
+                        </div>
                     </div>`
                 })
                 $details.html(content)
                 $goods = $('.goods')
+
                 $img = $goods.find('img');
                 $smallImg = $goods.next().find('img').eq(0);
                 console.log($smallImg);
@@ -41,9 +53,23 @@ require(['config'],function(){
                     // wihtd:400,
                     // height:400,
                     // position:'right'
-                    
+                });
                 
-                })
+                console.log(goods);
+                $buy = $('.buy')
+                
+                $buy.on('click','button',function(){
+                   
+                    let good_item = {
+                        id:goods[0].id,
+                        descirbe:goods[0].descirbe,
+                        price:goods[0].price,
+                        url:goods[0].url
+                    }
+                document.cookie = 'good_item'+JSON.stringify(good_item);
+                });
+                
+                
             }
         })
         
